ba73a1f90893854392ba651704cf4cc7d69e6392
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2021-11-07 12:38:32 -0600

    eslint work perfect

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello world from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello world from the middleware');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
