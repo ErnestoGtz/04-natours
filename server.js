0f76f99c68404b2e72f3660a1f253650946c9017
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2021-11-09 07:36:23 -0600

    new packages

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env' });
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const db = process.env.DB_CON.replace('<PASSWORD>', process.env.DB_PASS);
+mongoose
+  .connect(db, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log('DB is online'));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
