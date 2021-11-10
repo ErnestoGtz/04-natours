732cc5ecd001ae5d0c69d7fef116a1bd115421ce
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2021-11-10 08:25:52 -0600

    continue making better API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,8 +24,26 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Build the query
+    //1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((e) => delete queryObj[e]);
+    // console.log(req.query);
 
+    //2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    //console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // const tours = await Tour.find().where('duration').equals(5);
+
+    //execute the query
+    const tours = await query;
+
+    //Send Response
     res.status(200).json({
       status: 'Success',
       requestAt: req.requestTime,
