edd7dbd1a7d86658b9122cc36c354289fd5fc4e3
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2021-12-01 19:12:02 -0600

    continue with security

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'err',
-    message: 'This route is not yet define',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  //Send Response
+  res.status(200).json({
+    status: 'Success',
+    requestAt: req.requestTime,
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,6 +11,7 @@ const {
 } = require('../controllers/tourController');
 
 const router = express.Router();
+const authController = require('../controllers/authController');
 
 // router.param('id', checkID);
 router.route('/tour-stats').get(getTourStats);
@@ -19,7 +20,7 @@ router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(authController.protect, getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
