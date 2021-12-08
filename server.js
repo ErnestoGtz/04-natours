8f89ee24efd9486e8ba8f8a7f119129f79177999
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2021-12-08 17:28:32 -0600

    finished handling errors

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught exception! 🧨 Shutting down');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 
 const app = require('./app');
@@ -21,8 +27,8 @@ const server = app.listen(PORT, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('Unhandler rejection! 🧨 Shutting down');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
